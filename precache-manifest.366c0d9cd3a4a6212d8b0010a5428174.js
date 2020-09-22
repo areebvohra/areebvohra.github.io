8f0f5674b1802157f1e1e10950889f30d08cec51
areebvohra <areebvohra@gmail.com>
2020-09-22 22:34:58 +0500

    Initial commit

---
+++ precache-manifest.366c0d9cd3a4a6212d8b0010a5428174.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "15b79cb44b840a077568",
+    "url": "/static/js/main.15b79cb4.chunk.js"
+  },
+  {
+    "revision": "8c41a9e6d0b11a5a0c30",
+    "url": "/static/js/1.8c41a9e6.chunk.js"
+  },
+  {
+    "revision": "15b79cb44b840a077568",
+    "url": "/static/css/main.52eba9e7.chunk.css"
+  },
+  {
+    "revision": "e123d4faf25e786db56cb6069e88058f",
+    "url": "/index.html"
+  }
+];
---
+++ precache-manifest.59b68623fc39723c75b3899f2594d6f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6b1bf3a6e4ddc6212828",
+    "url": "/static/js/main.6b1bf3a6.chunk.js"
+  },
+  {
+    "revision": "8c41a9e6d0b11a5a0c30",
+    "url": "/static/js/1.8c41a9e6.chunk.js"
+  },
+  {
+    "revision": "6b1bf3a6e4ddc6212828",
+    "url": "/static/css/main.52eba9e7.chunk.css"
+  },
+  {
+    "revision": "15efe4bf03542d0b8e67f44383f079e1",
+    "url": "/index.html"
+  }
+];
